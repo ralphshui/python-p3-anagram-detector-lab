016345f2ebeef2738b910833091b4546405cb054
Ralph Shui <rskc.2112@gmail.com>
2023-07-26 20:43:12 -0400

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, testing_list):
+        anagram_list = []
+        sorted_word = sorted([letter for letter in self.word])
+        for testing_word in testing_list:
+            if sorted([letter for letter in testing_word]) == sorted_word:
+                anagram_list.append(testing_word)
+
+        return anagram_list
